7c0a80a2a391f21899d54837705fed0694790887
RaunakChhatwal <RaunakChhatwal001@gmail.com>
2026-01-26 07:36:38 -0500

    new init script

---
+++ .devcontainer/init.sh
@@ -0,0 +1,10 @@
+echo 'export PATH="$HOME/.nix-profile/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(/home/dev/.nix-profile/bin/direnv hook bash)"' >> ~/.bashrc
+echo 'export PS1="\u \W> "' >> ~/.bashrc
+source ~/.bashrc
+# nix profile add nixpkgs#python312
+cd /workspaces/pi-dsl
+echo 'use flake' >> .envrc
+/home/dev/.nix-profile/bin/direnv allow
+curl -fsSL https://opencode.ai/install | bash
+/home/dev/.nix-profile/bin/direnv exec . cabal update
