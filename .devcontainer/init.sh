ce7ff8acb2f6064dbdcd5747310ce420d2cddd8e
RaunakChhatwal <raunakchhatwal001@gmail.com>
2026-01-30 03:42:27 +0000

    configure vscode

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -8,3 +8,5 @@ echo 'use flake' >> .envrc
 /home/dev/.nix-profile/bin/direnv allow
 curl -fsSL https://opencode.ai/install | bash
 /home/dev/.nix-profile/bin/direnv exec . cabal update
+# code --install-extension haskell.haskell
+# code --install-extension mkhl.direnv
