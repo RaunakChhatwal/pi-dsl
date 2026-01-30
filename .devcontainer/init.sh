f428bdd6ec628cf902c7f1f17c6f9c4c5b2618ef
RaunakChhatwal <raunakchhatwal001@gmail.com>
2026-01-30 03:42:27 +0000

    configure vscode

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -2,7 +2,6 @@ echo 'export PATH="$HOME/.nix-profile/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(/home/dev/.nix-profile/bin/direnv hook bash)"' >> ~/.bashrc
 echo 'export PS1="\u \W> "' >> ~/.bashrc
 source ~/.bashrc
-# nix profile add nixpkgs#python312
 cd /workspaces/pi-dsl
 echo 'use flake' >> .envrc
 /home/dev/.nix-profile/bin/direnv allow
