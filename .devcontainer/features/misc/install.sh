2739ccf36deb8ccd28a1ce1b95594843cf654051
RaunakChhatwal <85747188+RaunakChhatwal@users.noreply.github.com>
2026-02-06 00:10:00 +0000

    install opencode as user 1000

--- .devcontainer/features/misc/install.sh
+++ .devcontainer/features/misc/install.sh
@@ -3,7 +3,7 @@ set -e
 
 # Install python
 nix profile add nixpkgs#python312
+export PATH="/home/dev/.nix-profile/bin:$PATH"
 
 # Install opencode
-curl -fsSL https://opencode.ai/install | bash
-export PATH="/home/dev/.nix-profile/bin:$PATH"
+su dev -c "curl -fsSL https://opencode.ai/install | bash"
--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -1,6 +1,6 @@
 direnv=/home/dev/.nix-profile/bin/direnv
 echo 'export PATH="$HOME/.nix-profile/bin:$PATH"' >> ~/.bashrc
-echo 'eval "$($direnv hook bash)"' >> ~/.bashrc
+echo "eval \"\$($direnv hook bash)\"" >> ~/.bashrc
 echo 'export PS1="\u \W> "' >> ~/.bashrc
 source ~/.bashrc
 cd /workspaces/pi-dsl
